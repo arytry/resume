2950aad672e6b2267e382f735b082e0229970957
Linkjr <linkjr@sina.com>
2021-04-28 16:48:03 +0800

    use first directory level.

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
-    base: '/resume/',
+    base: '/',
     locales: {
         '/': {
             lang: 'en-US',
